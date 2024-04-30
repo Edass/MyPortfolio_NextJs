279c7bbaaa88ada9eeb744a460466f5105a8d3fd
Eda Sarıkaya <edassarkaya@outlook.com>
2024-04-30 17:08:32 +0100

    added

--- src/components/common/footer/footer.jsx
+++ src/components/common/footer/footer.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import SocialMedia from "../header/social-media";
+import "./footer.scss";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="footer">
+    <Container >
+      <Row className="p-2 d-flex justify-content-center align-items-center">
+        <Col className="d-flex justify-content-center align-items-center">
+          <h4>Eda Sarikaya</h4>{" "}
+        </Col>
+        <Col className="d-flex justify-content-center align-items-center">
+          <SocialMedia gap="4" />{" "}
+        </Col>
+        <Col className="d-flex justify-content-center align-items-center">
+          {" "}
+          &copy; by edasarikaya
+        </Col>
+      </Row>
+    </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/common/header/social-media.jsx
+++ src/components/common/header/social-media.jsx
@@ -1,12 +1,12 @@
 import { FaGithub,FaLinkedin } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
 import "./social-media.scss";
-const SocialMedia = () => {
+const SocialMedia = ({flex="row", gap="4"}) => {
   return (
-    <div className="medias">
-    <a href=""><FaGithub /></a>
-    <a href=""><FaLinkedin /></a>
-    <a href=""><IoMdMail /></a>
+    <div className={`medias d-flex flex-${flex} gap-${gap}`}>
+    <a href="https://github.com/Edass" target="_blank"><FaGithub /></a>
+    <a href="https://www.linkedin.com/in/eda-sarikaya/" target="_blank"><FaLinkedin /></a>
+    <a href="mailto: edassarkaya@outlook.com" target="_blank"><IoMdMail /></a>
     </div>
   )
 }
